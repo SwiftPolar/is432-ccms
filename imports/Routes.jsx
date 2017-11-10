a44eabf35be5d72cfce15320cb5e29a35ebde126
Lee Qixian <qixian@leeqixian.com>
2017-11-10 18:39:56 +0800

    created standard template for the ccms dashboard

--- imports/Routes.jsx
+++ imports/Routes.jsx
@@ -3,12 +3,14 @@ import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom
 import {createContainer} from 'meteor/react-meteor-data';
 
 import PublicApp from './ui/public/App';
+import MomApp from './ui/mom/App';
 
 export const RenderRoutes = () => (
     <Router>
         <div><Switch>
             <Route exact path="/" component={PublicApp}/>
             <Route path="/public" component={PublicApp}/>
+            <Route path="/mom" component={MomApp} />
         </Switch></div>
     </Router>
 )
